6527ed680531a1ec0265a0edc1d755b83ee46b9c
root <root@stfc-tests.novalocal>
2019-08-14 16:29:09 +0200

    updated hello-world.sh and cau-client.sh

--- docker-compose/tests/cau-client.sh
+++ docker-compose/tests/cau-client.sh
@@ -31,18 +31,19 @@ else
 	echo "cau-client container id : ${cid}"
 fi
 
+#Actually, we cannot call the get certificate method as it has side effects.  The certificate is stored in the keystore and written to the /pkidata file volume
 #set up request
-getcert="detectedLeaderID=0f848d8fb78cbe5615507ef5a198f660ac89a3ae03b95e79d4ebfb3466c20d54e9a5d9b9c41f88c782d1f67b32231d31b4fada8d2f9dd31a4d884681b784ec5a,deviceID=c6968d75a7df20e2d2f81f87fe69bf0b7dd14f4a22cca5f15ffc645cb4d45944bfdc7a7a970a9e13a331161e304a3094d8e6e362e88bd7df0d7b5473b6d2aa80,IDkey=888812345"
+#getcert="detectedLeaderID=0f848d8fb78cbe5615507ef5a198f660ac89a3ae03b95e79d4ebfb3466c20d54e9a5d9b9c41f88c782d1f67b32231d31b4fada8d2f9dd31a4d884681b784ec5a,deviceID=c6968d75a7df20e2d2f81f87fe69bf0b7dd14f4a22cca5f15ffc645cb4d45944bfdc7a7a970a9e13a331161e304a3094d8e6e362e88bd7df0d7b5473b6d2aa80,IDkey=888812345"
 
 
-cert=$(docker exec -it "${cid}" sh -c "echo -n ${getcert} | nc localhost 46065")
+#cert=$(docker exec -it "${cid}" sh -c "echo -n ${getcert} | nc localhost 46065")
 #cau-client returns "OK" if it worked
-echo "get cert returned : ${cert}"
-if [ -z "${cert}" ] || [ "${cert:0:2}" == "err" ]; then
-	log "FAILED" "Failed to get cert from cau-client" 
-else
-	log "OK" "Got cert from cau-client"
-fi
+#echo "get cert returned : ${cert}"
+#if [ -z "${cert}" ] || [ "${cert:0:2}" == "err" ]; then
+#	log "FAILED" "Failed to get cert from cau-client" 
+#else
+#	log "OK" "Got cert from cau-client"
+#fi
 #get public key
 getpk="getpubkey=c6968d75a7df20e2d2f81f87fe69bf0b7dd14f4a22cca5f15ffc645cb4d45944bfdc7a7a970a9e13a331161e304a3094d8e6e362e88bd7df0d7b5473b6d2aa80"
 pubkey=$(docker exec -it "${cid}" sh -c "echo -n ${getpk} | nc localhost 46065")
