ea0fe1dda61df6e6d5ee749c254596f8b9ffe035
Alejandro Gomez Cardenas <alejandg@ac.upc.edu>
2019-10-03 07:07:27 -0700

    Update identification.sh

--- docker-compose/tests/identification.sh
+++ docker-compose/tests/identification.sh
@@ -20,17 +20,20 @@ endpoint="http://localhost:46060/api/v1/resource-management/identification/reque
 
 
 # Is the device already registered?
-ans=`curl -s -XGET ${endpoint} | jq -r '.status'`
-if [[ ${ans} -eq 412 ]]; then
-    log "NO" "Device isn't registered yet"
-else
-    log "OK" "The device is registered"
-fi
-
-# Get new device ID
 ans=`curl -s -GET ${endpoint} | jq -r '.status'`
 if [[ ${ans} -eq 200 ]]; then
-    log "OK" "Device ID has been successfully obtained"
+    log "OK" "The device is registered"
+    registered="OK"
 else
-    log "NO" "Failed to obtain the device ID"
+    log "NO" "Device isn't registered yet"
+    registered="NO"
+fi
+
+# Get the stored IDkey and deviceID
+if [[ ${registered} == "OK" ]]; then
+    IDKey=`curl -s -GET ${endpoint} | jq -r '.IDKey'`
+    IDKey=${IDKey:0:8}
+    deviceID=`curl -s -GET ${endpoint} | jq -r '.deviceID'`
+    deviceID=${deviceID:0:8}
+    printf '\e[0;33m %-15s \e[0;36m INFO:\e[0m %s \n' [Identification-Test] "The truncated IDKey string is ${IDKey} and the truncated deviceID string is ${deviceID}"
 fi
