e5b56a9c22a24fc99b20e4352fb8399c1ed02ccb
ALEJANDROJ19 <ALEJANDROJ19@hotmail.es>
2019-10-17 12:22:26 +0200

    identification fix issue

--- docker-compose/tests/identification.sh
+++ docker-compose/tests/identification.sh
@@ -5,6 +5,8 @@
 printf '\e[0;33m %-15s \e[0m \n' "The installation of the jq package is a prerequisite to run the test"
 printf '\e[0;33m %-15s \e[0m Starting...\n' [Identification-Test]
 
+DOCKER_NAME_IDENTIFICATION="identification"
+
 function log
 {
     text="$2"
@@ -16,11 +18,17 @@ function log
     fi
 }
 
+function exec_docker {
+    container_name=$(docker ps -f "name=$DOCKER_NAME_IDENTIFICATION" --format '{{.Names}}')
+    exec_res=$(docker exec "${container_name}" /bin/bash -c "$1"  2>/dev/null)
+}
+
 endpoint="http://localhost:46060/api/v1/resource-management/identification/requestID"
 
 
 # Is the device already registered?
-ans=`curl -s -GET ${endpoint} | jq -r '.status'`
+exec_docker $endpoint
+ans=`echo -e "${exec_res}" | jq -r '.status'`
 if [[ ${ans} -eq 200 ]]; then
     log "OK" "The device is registered"
     registered="OK"
@@ -31,9 +39,9 @@ fi
 
 # Get the stored IDkey and deviceID
 if [[ ${registered} == "OK" ]]; then
-    IDKey=`curl -s -GET ${endpoint} | jq -r '.IDKey'`
+    IDKey=`echo -e "${exec_res}" | jq -r '.IDKey'`
     IDKey=${IDKey:0:8}
-    deviceID=`curl -s -GET ${endpoint} | jq -r '.deviceID'`
+    deviceID=`echo -e "${exec_res}" | jq -r '.deviceID'`
     deviceID=${deviceID:0:8}
     printf '\e[0;33m %-15s \e[0;36m INFO:\e[0m %s \n' [Identification-Test] "The truncated IDKey string is ${IDKey} and the truncated deviceID string is ${deviceID}"
 fi
