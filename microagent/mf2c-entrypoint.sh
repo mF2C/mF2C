9b49bfa957d0a2ac509860de81a7d8362e1be4e8
Cristovao Cordeiro <cristovao.cordeiro@sixsq.com>
2020-01-15 08:12:17 +0100

    Update mf2c-entrypoint.sh
    increase timeout

--- microagent/mf2c-entrypoint.sh
+++ microagent/mf2c-entrypoint.sh
@@ -70,10 +70,10 @@ done
 
 cau_registration="echo detectedLeaderID=${detectedLeaderID},deviceID=${deviceID} | nc localhost 46065"
 
-timeout -t 10 docker exec mf2c_micro_cau-client sh -c "${cau_registration}"
+timeout -t 30 docker exec mf2c_micro_cau-client sh -c "${cau_registration}"
 while [ $? -ne 0 ]
 do
-  timeout -t 10 docker exec mf2c_micro_cau-client sh -c "${cau_registration}"
+  timeout -t 60 docker exec mf2c_micro_cau-client sh -c "${cau_registration}"
 done
 
 docker run -d --network="host" \
